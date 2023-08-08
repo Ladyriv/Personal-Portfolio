d6b2a3ded2d73b0498fedc5e3d12c7f42763f43e
Leidy Rivera <leidy.riveraf@outlook.com>
2023-08-08 08:30:18 -0500

    fix module

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import type { NextPage } from 'next'
 //import Head from 'next/head'
-import styles from '@/styles/globals.css';
+import styles from '../styles/globals.css';
 import { useForm } from 'react-hook-form';
 import { Link } from "react-scroll/modules"
 
